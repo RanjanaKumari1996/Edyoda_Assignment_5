20ba1264f67b8b8d17dfc29fbcb0476c7fbf8db1
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-10-14 20:27:51 +0530

    Add files via upload

---
+++ _5_Assignment/_2_Implement_a_calculator_class.py
@@ -0,0 +1,35 @@
+from re import X
+
+
+class Calculator:
+
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    def add(self):
+        self.sum = self.y + self.x
+        return self.sum 
+
+    def subtract(self):
+        self.subt = self.y - self.x
+        return self.subt
+
+    def multiply(self):
+        self.mul = self.y * self.x
+        return self.mul
+
+    def divide(self):
+        self.div = self.y / self.x
+        return self.div
+
+
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+print(end="\n")
+
+calculator_obj=Calculator(x,y)
+print("Addition of numbers: ",calculator_obj.add())
+print("Subtraction of numbers: ",calculator_obj.subtract())
+print("Multiplication of numbers: ",calculator_obj.multiply())
+print("Division of numbers: ",calculator_obj.divide())
